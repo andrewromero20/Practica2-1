dabbf7cb18f64800e316c761e13a9e14ec86349f
wildbearbaux <alfredvs14@gmail.com>
2025-10-04 17:36:37 -0600

    Versión de algoritmo de amplitud integrado

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
+import numpy as np
 from ocho_puzzle import MatrizOchoRompeCabezas
 from dfs_Algoritmo import dfs_resolver
-import numpy as np
+from bfs_Algoritmo import bfs_resolver
 
+# Crear puzzle aleatorio
 p = MatrizOchoRompeCabezas()
 
-meta = np.array([[1,2,3],
-                 [4,5,6],
-                 [7,8,0]])
+print("=== ESTADO INICIAL ===")
+print(p.matriz)
 
-dfs_resolver(p, meta, max_profundidad=30)
+meta = np.array([[1, 2, 3],
+                 [4, 5, 6],
+                 [7, 8, 0]])
+
+print("\n=== ELIGE ALGORITMO ===")
+print("1. Profundidad (DFS)")
+print("2. Amplitud (BFS)")
+opcion = input("Opcion: ")
+
+print("\n=== RESULTADO ===")
+if opcion == "1":
+    dfs_resolver(p, meta, max_profundidad=30)
+elif opcion == "2":
+    bfs_resolver(p, meta)
+else:
+    print("Opcion no valida.")
